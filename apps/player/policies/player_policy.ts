38b24691f6a7ea08fcd68f005584abe169b3da2e
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-12-08 22:01:51 +0100

    feat: update method in `PlayerPolicy`
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- apps/player/policies/player_policy.ts
+++ apps/player/policies/player_policy.ts
@@ -2,16 +2,31 @@ import { AuthorizationResponse, BasePolicy } from '@adonisjs/bouncer'
 import { JWTPayload } from '#apps/authentication/contracts/jwt'
 import { inject } from '@adonisjs/core'
 import RoleService from '#apps/shared/services/role_service'
+import PlayerService from '#apps/player/services/player_service'
 
 @inject()
 export default class PlayerPolicy extends BasePolicy {
-  constructor(protected roleService: RoleService) {
+  constructor(
+    protected roleService: RoleService,
+    protected playerService: PlayerService
+  ) {
     super()
   }
 
-  async view(payload: JWTPayload) {
+  async view(payload: JWTPayload, playerId?: string) {
+
     if (this.roleService.hasRole(payload, 'view-player')) {
       return AuthorizationResponse.allow()
+    }
+
+    if (playerId) {
+      const player = await this.playerService.findByOidcId(payload.sub).catch(() => {
+        return null
+      })
+
+      if (player && player.id === playerId) {
+        return AuthorizationResponse.allow()
+      }
     }
 
     return AuthorizationResponse.deny('You are not allowed to view a player')
